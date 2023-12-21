f34e1c49fc97508c1d3af82783710f9c2ce25a48
Shivam Rana <shivamrana704.sr@gmail.com>
2023-12-21 13:15:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
+import styles from "./App.module.css";
+import Display from "./components/displayBox";
+import buttons from "./components/buttonItems";
 
 function App() {
-
   return (
-    <>
-      <div className="calculator">
-        kdslnf
-      </div>
-    </>
-  )
+    <div className={styles.calculator}>
+      <Display></Display>
+      <buttons></buttons>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/displayBox.jsx
@@ -0,0 +1,9 @@
+import styles from "./displayBox.module.css"
+
+let Display=()=>{
+  return (
+    <input className={styles.display} type="text"
+    />
+  )
+}
+export default Display;
